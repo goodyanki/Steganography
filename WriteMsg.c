6a4396071c13233734b7aa46c13c9c267f7e4c0a
rakknnen <tup78242@temple.edu>
2024-11-18 14:44:53 +0900

    1.1

--- WriteMsg.c
+++ WriteMsg.c
@@ -7,15 +7,22 @@ int main(int argc, char *argv[])
         printf("More argument required");
     }
 
-    if(argc = 3)
+    if(argc == 3)
     {
         FILE *fp = NULL;
         fp = fopen(argv[2], "r");
         char str[255];
         int i = 0;
-        int lineCount = 1;
+        int lineCount = 0;
+        if (fp == NULL) 
+        {
+            perror("Error opening file");
+            return 1; 
+        }
         while(fgets(str, 255, fp) != NULL)
         {
+            lineCount += 1;
+
             if (lineCount == 1)
             { 
                 if(str[0] ==  'P' && str[1] == '6')
@@ -27,26 +34,26 @@ int main(int argc, char *argv[])
                     perror("IT IS NOT A PPM FILE.\n");
                 }
             }
-            lineCount += 1;
 
             if(lineCount == 2)
             {
                 if(str[0] == '#')
                 {
                     printf("hello\n");
-                    continue;
+                    
                 }
                 else
                 {
-                    printf("%s", str[0]);
+                    printf("%c", str[0]);
                     printf("hi\n");
                 }
             }
 
             
         }
-
+        fclose(fp);
     }
 
+    
     return 0;
 }
